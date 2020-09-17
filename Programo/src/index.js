c1ec6bdf7eb5f2e20ccdc228d5e52a5a5e08b26f
Omar Guzmán <33527049+jomarguzman@users.noreply.github.com>
2020-09-16 22:23:49 -0500

    Add files via upload

--- Programo/src/index.js
+++ Programo/src/index.js
@@ -1,68 +1,50 @@
-//npm install express
-var express = require('express');
-//npm install body-parser
-var bodyParser = require('body-parser');
-//npm install jsonwebtoken
-var jwt = require('jsonwebtoken');
-//npm install express-jwt
-var expressJwt = require('express-jwt');
+//Agregar express, body-parser, jsonwebtoken, express-jwt
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
 
-var app = express();
+const app = express();
 
-//creamos una clave para la incriptacion del token
-var jwtClave = "Password_deseado";
-
+//clave para la encriptacion
+const jwtClave = "jwt_programo";
 
 app.use(express.static('publica'));
 app.use(bodyParser.json());
 
-//Protegemos todo menos el /login
+//protegemos todo menos el /login
+app.use(expressJwt({ secret: jwtClave, algorithms: ['HS256'] }).unless({ path: ["/login"] }));
 
-app.use(expressJwt({ secret: jwtClave }).unless({ path: ["/login"] }));
-
-//creamos un usaurio y una contraseña 
-var usuario = {
+//crear usr y pwd
+let usuario = {
     nombre: "usuario",
     clave: "password"
 }
 
-// Creamos una noticia  como si bieiera de una base para mostrarlo en el metodo protegido
-var noticias = [{
+//noticias
+let noticias = [{
     id: 1,
     titulo: "noticia 1"
 }];
 
-//Metodo protegido con to jwt
-app.get('/noticias', function (req, res) {
+app.get('/noticias', function(req, res){
     res.send(noticias);
-});
+})
 
-app.get('/protected', function (req, res) {
-    console.log(req.body);
-    if (!req.body.usuario) return res.sendStatus(401);
-    res.send('OK');
-});
-
-
-//Creamos el endpoint para el login 
-app.post("/login", function (request, response) {
-    console.log(request.body);
+app.post('/login', function(req, res){
     if (request.body.nombre == usuario.nombre || request.body.clave == usuario.clave) {
-        //Creamos el token para pasar
-        var token = jwt.sign({
-            usuario: "usuario"
-        }, jwtClave, { expiresIn: 300 });
-        
-        //envio Token
-        response.send(token);
+        //crear token
+        let token = jwt.sign({
+            usuario:"usuario"
+        },jwtClave);   
 
-    } else {
-
-        response.status(401).end("usuario incorrecto")
+        //enviar token respuesta
+        res.send(token);
+    } else {   
+        res.status(401).end("usuario incorrecto")
     }
-
 });
 
-app.listen(3002, function () {
+app.listen(3050, function(){
     console.log('aplicacion en el puerto 3000!');
-});
+})
