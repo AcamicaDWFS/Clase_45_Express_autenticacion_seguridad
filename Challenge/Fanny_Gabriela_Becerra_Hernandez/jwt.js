9c716973ecba234e340fa9e747e2206e96a5ba41
fannygabriela.becerra <fannygabriela.becerra@bbva.com>
2020-09-17 18:07:38 -0500

    Clase 45: Challenge

--- Challenge/Fanny_Gabriela_Becerra_Hernandez/jwt.js
+++ Challenge/Fanny_Gabriela_Becerra_Hernandez/jwt.js
@@ -2,14 +2,16 @@ var jwt = require('jsonwebtoken');
 var privateKey = '123';
 var token = jwt.sign({
     exp: Math.floor(Date.now() / 1000) + (60 * 60),
-    data: 'foobar'
+    data: '¡Holi! :3'
 }, 'secret');
 
-console.log('token: ', token);
-
-try {
-    var decoded = jwt.verify(token, 'secret');
-    console.log('\n\nSuccess!')
-} catch (err) {
-    console.log('Error: ', err)
+if (token) {
+    console.log('token: ', token);
+    try {
+        var decoded = jwt.verify(token, 'secret');
+        console.log('\nSuccess!')
+        console.log('\ndecoded: ', decoded);
+    } catch (err) {
+        console.log('Error: ', err)
+    }
 }
