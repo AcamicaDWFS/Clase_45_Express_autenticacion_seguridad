3d1e70b62fd7dabf041f134ef102f0658cb5576d
Omar Guzmán <33527049+jomarguzman@users.noreply.github.com>
2020-09-16 22:22:23 -0500

    Add files via upload

--- Programo/src/index.js
+++ Programo/src/index.js
@@ -1,49 +1,68 @@
-//Agregar express, body-parser, jsonwebtoken, express-jwt
-const express = require('express');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const expressJwt = require('express-jwt');
+//npm install express
+var express = require('express');
+//npm install body-parser
+var bodyParser = require('body-parser');
+//npm install jsonwebtoken
+var jwt = require('jsonwebtoken');
+//npm install express-jwt
+var expressJwt = require('express-jwt');
 
-const app = express();
+var app = express();
 
-//clave para la encriptacion
-const jwtClave = "jwt_programo";
+//creamos una clave para la incriptacion del token
+var jwtClave = "Password_deseado";
 
+
+app.use(express.static('publica'));
 app.use(bodyParser.json());
 
-//protegemos todo menos el /login
-app.use(expressJwt({secret: jwtClave}).unless({path:["/login"]}));
+//Protegemos todo menos el /login
 
-//crear usr y pwd
-let usuario = {
+app.use(expressJwt({ secret: jwtClave }).unless({ path: ["/login"] }));
+
+//creamos un usaurio y una contraseña 
+var usuario = {
     nombre: "usuario",
     clave: "password"
 }
 
-//noticias
-let noticias = [{
+// Creamos una noticia  como si bieiera de una base para mostrarlo en el metodo protegido
+var noticias = [{
     id: 1,
     titulo: "noticia 1"
 }];
 
-app.get('/noticias', function(req, res){
+//Metodo protegido con to jwt
+app.get('/noticias', function (req, res) {
     res.send(noticias);
-})
-
-app.post('/login', function(req, res){
-    if (request.body.nombre == usuario.nombre || request.body.clave == usuario.clave) {
-        //crear token
-        let token = jwt.sign({
-            usuario:"usuario"
-        },jwtClave);   
-
-        //enviar token respuesta
-        res.send(token);
-    } else {   
-        res.status(401).end("usuario incorrecto")
-    }
 });
 
-app.listen(3005, function(){
+app.get('/protected', function (req, res) {
+    console.log(req.body);
+    if (!req.body.usuario) return res.sendStatus(401);
+    res.send('OK');
+});
+
+
+//Creamos el endpoint para el login 
+app.post("/login", function (request, response) {
+    console.log(request.body);
+    if (request.body.nombre == usuario.nombre || request.body.clave == usuario.clave) {
+        //Creamos el token para pasar
+        var token = jwt.sign({
+            usuario: "usuario"
+        }, jwtClave, { expiresIn: 300 });
+        
+        //envio Token
+        response.send(token);
+
+    } else {
+
+        response.status(401).end("usuario incorrecto")
+    }
+
+});
+
+app.listen(3002, function () {
     console.log('aplicacion en el puerto 3000!');
-})
+});
